f02cfa9ddb8768622a610e2dd8ad975cf7225dd9
Muhammad Ali Makhdoom <14muhammad>
2018-09-03 22:07:11 +0500

    admin login page

---
+++ admin/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+    <title>Admin Login</title>
+</head>
+<body class="text-center">
+    <form class="login_form" action="" method="post">
+        <h3 class="m-3">Admin Login </h3>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary mt-3" type="submit">Sign in</button>
+    </form>
+<script src="assets/js/jquery-3.3.1.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
