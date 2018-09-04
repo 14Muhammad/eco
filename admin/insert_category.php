7b143dc53fbc978e873e62ffafedcde928d43fc7
Fahad Ali <fahadali812.fa@gmail.com>
2018-09-04 18:35:55 +0500

    add file of insert category

---
+++ admin/insert_category.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fahad Ali
+ * Date: 9/4/2018
+ * Time: 6:31 PM
+ */
+?>
+
+<form action="" method="post" style="padding: 140px; background-color: steelblue;">
+
+    <b>Insert New Category:</b>
+    <input type="text" name="new_cat" required/>
+    <input type="submit" name="add_cat" value="Add Category" />
+
+</form>
