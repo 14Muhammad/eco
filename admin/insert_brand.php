bc9eabe0696166cc2f4e1bd0b046fefc364cb717
Fahad Ali <fahadali812.fa@gmail.com>
2018-09-04 12:41:16 +0500

    add file of insert Brand

---
+++ admin/insert_brand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fahad Ali
+ * Date: 9/4/2018
+ * Time: 12:35 PM
+ */
+?>
+<form action="" method="post" style="padding:80px;">
+
+<b>Insert New Bran:</b>
+<input type="text" name="new_brand" required/>
+<input type="submit" name="add_brand" value="Add Brand" />
+
+</form>
