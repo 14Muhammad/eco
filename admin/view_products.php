d668b99ac34bd6a65026caa5e3e6ed33088a983a
Fahad Ali <fahadali812.fa@gmail.com>
2018-09-06 16:38:55 +0500

    Error occure in database conection error is (Undefined variable: con in)

--- admin/view_products.php
+++ admin/view_products.php
@@ -22,33 +22,25 @@
         </thead>
         <tbody>
         <?php
-
         $get_pro = "select * from products";
-
-        $run_pro = mysqli_query($con, $get_pro);
-
-        $i = 0;
-
-        while ($row_pro=mysqli_fetch_array($run_pro)){
-
-            $pro_id = $row_pro['pro_id'];
-            $pro_title = $row_pro['pro_title'];
+        $run_pro = mysqli_query($con, $get_pro);// here is error: Undefined variable: con in
+        while($row_pro= mysqli_fetch_array($run_pro))
+        {
+            $pro_id= $row_pro['pro_id'];
             $pro_cat = $row_pro['pro_cat'];
+            $pro_title = $row_pro['pro_title'];
+            $pro_price = $row_pro['peo_price'];
             $pro_image = $row_pro['pro_image'];
-            $pro_price = $row_pro['pro_price'];
-            $i++;
+            echo "<tr>
+                      <td>$pro_id</td>
+                      <td>$pro_cat</td>
+                      <td>$pro_title</td>
+                      <td>$pro_price</td>
+                     <td><img src='../admin/product_images/$image' width='50px' height='50px'></td>
+                  </tr>";
+        }
+        ?>
 
-            ?>
-            <tr align="center">
-                <td><?php echo $i;?></td>
-                <td><?php echo $pro_title;?></td>
-                <td><img src="product_images/<?php echo $pro_image;?>" width="60" height="60"/></td>
-                <td><?php echo $pro_price;?></td>
-                <td><a href="index.php?edit_pro=<?php echo $pro_id; ?>">Edit</a></td>
-                <td><a href="delete_pro.php?delete_pro=<?php echo $pro_id;?>">Delete</a></td>
-
-            </tr>
-        <?php } ?>
         </tbody>
     </div>
 </table>
