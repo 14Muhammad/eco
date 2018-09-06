4b1ad582c9d9a5eb78f550e54fc381a57950dcc6
alimakhdoom <ali.makhdoom@ucp.edu.pk>
2018-09-06 20:02:06 +0500

    brand responsive centralized

--- admin/view_brands.php
+++ admin/view_brands.php
@@ -1,12 +1,16 @@
-<h1> Brands </h1>
-<ul class="list-group">
-    <?php
-    $get_brands = "select * from brands";
-    $run_brands = mysqli_query($con, $get_brands);
-    while ($row_brands= mysqli_fetch_array($run_brands)){
-        $brand_id = $row_brands['brand_id'];
-        $brand_title = $row_brands['brand_title'];
-        echo "<li class='list-group-item'>$brand_title</li>";
-    }
-    ?>
-</ul>
+<div class="row">
+    <div class="col-sm-6 offset-sm-3">
+        <h1> Brands </h1>
+        <ul class="list-group">
+            <?php
+            $get_brands = "select * from brands";
+            $run_brands = mysqli_query($con, $get_brands);
+            while ($row_brands= mysqli_fetch_array($run_brands)){
+                $brand_id = $row_brands['brand_id'];
+                $brand_title = $row_brands['brand_title'];
+                echo "<li class='list-group-item'>$brand_title</li>";
+            }
+            ?>
+        </ul>
+    </div>
+</div>
