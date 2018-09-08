6d47ee29ea1b19beb9843945c5fe48f651171476
Muhammad Ali Makhdoom <14muhammad>
2018-09-09 01:53:10 +0500

    admin panel view/edit/delete categories & brands

--- admin/view_categories.php
+++ admin/view_categories.php
@@ -1,16 +1,38 @@
 <div class="row">
-    <div class="col-sm-6 offset-sm-3">
-        <h1> Categories </h1>
-        <ul class="list-group">
+    <div class="col-sm-12">
+        <h1>Categories</h1>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
             <?php
             $get_cats = "select * from categories";
             $run_cats = mysqli_query($con, $get_cats);
+            $i=0;
             while ($row_cats= mysqli_fetch_array($run_cats)){
                 $cat_id = $row_cats['cat_id'];
                 $cat_title = $row_cats['cat_title'];
-                echo "<li class='list-group-item'>$cat_title</li>";
+                ?>
+                <tr>
+                    <th scope="row"><?php echo ++$i; ?></th>
+                    <td><?php echo $cat_title; ?></td>
+                    <td><a href="index.php?edit_cat=<?php echo $cat_id?>" class="btn btn-primary">
+                            <i class="fa fa-edit"></i> Edit
+                        </a>
+                        <a href="index.php?del_cat=<?php echo $cat_id?>" class="btn btn-danger">
+                            <i class="fa fa-trash-alt"></i> Delete
+                        </a>
+                    </td>
+                </tr>
+                <?php
             }
             ?>
-        </ul>
+            </tbody>
+        </table>
     </div>
 </div>
